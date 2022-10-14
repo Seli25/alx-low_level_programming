5e2b08ad0f4a40db25e8ac45b8e8b4e8c2855234
Blessed <blessediyogun25@gmail.com>
2022-10-14 18:58:11 +0000

    Q5v2

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,12 @@
 /**
  * main - main function of program
  *
- * Retun: Always returns 0
+ * Retun: Always 0
  */
 
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 }

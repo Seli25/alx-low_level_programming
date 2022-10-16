197a8008bb70cccaff13837438a2b3b5886de1fc
Blessed <blessediyogun25@gmail.com>
2022-10-16 18:15:12 +0000

    Q2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main function of the program
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char letter;
+	
+	for (letter = a; letter <= z; letter++) 
+		
+		putchar(letter);
+	
+	putchar('\n');
+	
+	return (0);
+}

53f04ca44298465c5ae76c32f0230a3905b97975
Blessed <blessediyogun25@gmail.com>
2022-10-17 18:02:14 +0000

    Q5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - main function of program
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+
+		printf("%d", num);
+
+	printf("\n");
+
+	return (0);
+
+}
+

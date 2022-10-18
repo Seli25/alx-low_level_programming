5e9b5b199b1a8ad525de796e15a8844518e009ae
Blessed <blessediyogun25@gmail.com>
2022-10-18 19:51:26 +0000

    Q6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _abs - program that prints the absolute value of a number
+ *
+ * @n: value being used in the program
+ *
+ * Return: Returns 0
+ */
+
+int _abs(int n)
+{
+	int absn;
+
+	if (n < 0)
+	{
+		result = n * -1;
+		return (absn);
+	}
+
+	else
+	{
+		result = n;
+		return (absn);
+	}
+
+	return (0);
+}

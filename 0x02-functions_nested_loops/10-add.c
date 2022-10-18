c1ead5f028867be752be76599f90f92bc93c3e31
Blessed <blessediyogun25@gmail.com>
2022-10-18 20:37:29 +0000

    Q9

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * add - function add two values
+ *
+ * @i: value to be used
+ *
+ * @j: value to be used
+ *
+ * Return: returns the sum
+ */
+
+int add(int i, int j)
+{
+	int sum;
+
+	sum = i + j;
+
+	return (sum);
+}

7d00992a4d345464097bce455610e8aa93de2132
Blessed <blessediyogun25@gmail.com>
2022-10-18 18:44:57 +0000

    Q3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _islower - function to check if the letter is lewwer case
+ *
+ * @c: the value used in the function
+ *
+ * Return: void function.
+ */
+
+int _islower(int c)
+{
+	int i = 'a';
+
+	for (i = 'a'; i < 'z'; i++)
+	{
+		if (c == 1)
+		{
+			return (1);
+		}
+		else
+			return (0);
+	}
+
+	return (0);
+}

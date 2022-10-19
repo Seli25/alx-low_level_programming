79178f5161fe680976d5d8f22fd5bccb43789305
Blessed <blessediyogun25@gmail.com>
2022-10-19 19:08:22 +0000

    Q16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - main function that print the first 98
+ * fibonaci numbers starting from 1 and 2
+ *
+ * Return: Retuns 0:
+ */
+
+int main(void)
+{
+	long int n1 = 1, n2 = 2, num = 3;
+	long int nextnum = n1 + n2;
+
+	printf("%lu, ", n1);
+	printf("%lu, ", n2);
+
+	while (num <= 98)
+	{
+		if (num == 98)
+		{
+			printf("%lu\n", nextnum);
+		}
+
+		else
+		{
+			printf("%lu, ", nextnum);
+		}
+
+		n1 = n2;
+		n2 = nextnum;
+
+		nextnum = n1 + n2;
+		num++;
+	}
+
+	return (0);
+}

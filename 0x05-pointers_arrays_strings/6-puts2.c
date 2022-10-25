772176a7edbdb7e8cfd87c52968a56d8a68310dd
Blessed <blessediyogun25@gmail.com>
2022-10-25 18:01:25 +0000

    Q6v3

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -13,11 +13,13 @@ void puts2(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		for (j = 0; j < i; j += 2)
-		{
-			_putchar(str[j]);
-		}
-
-		_putchar('\n');
+		;
 	}
+	
+	for (j = 0; j < i; j += 2)
+	{
+		_putchar(str[j]);
+	}
+
+	_putchar('\n');
 }
